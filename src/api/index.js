550387010aa3ff3aebd8c9dde171efda350b5075
successfulcode <sergej.mickevic@gmail.com>
2021-07-11 20:06:15 +0300

    Updated cors service

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const url = process.env.VUE_APP_BASE_URL;
-// const cors = process.env.VUE_APP_CORS;
+const cors = process.env.VUE_APP_CORS;
 
 const ApiService = {
   getCategories() {
-    return axios.get(`${url}/categories`);
+    return axios.get(`${cors}${url}/categories`);
   },
   getProducts(categoryId) {
-    return axios.get(`${url}/products/category/${categoryId}`);
+    return axios.get(`${cors}${url}/products/category/${categoryId}`);
   }
 };
 
